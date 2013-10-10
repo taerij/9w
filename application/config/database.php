ce6f340a66ff2f03a7ee1013a7b0cf1c28f9c438
taerij <taerij@yahoo.com>
2013-10-10 12:54:36 +0100

    Config DB

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'cl49-9w';
+$db['default']['password'] = '2qEqe2!T3';
+$db['default']['database'] = 'cl49-9w';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
